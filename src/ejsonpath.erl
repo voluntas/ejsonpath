1d0698a738ffea2158a40da79cfb17e932660949
NAKAI Ryosuke <nakai@shiguredo.jp>
2018-05-14 17:50:16 +0900

    Fix

--- src/ejsonpath.erl
+++ src/ejsonpath.erl
@@ -170,11 +170,11 @@ slice_list([Idx | Rest], L, Len) when Idx < 0 ->
 slice_list([Idx | Rest], L, Len) ->
     %% そもそも ID が存在しない場合の処理が入っていない
     case length(L) of
-        Length when Legnth >= (Idex + 1) ->
+        Length when Length >= (Idx + 1) ->
             [lists:nth(Idx + 1, L) | slice_list(Rest, L, Len)];
         _ ->
             []
-    end
+    end;
 slice_list([], _, _) ->
     [].
 
